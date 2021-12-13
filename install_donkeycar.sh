e78025b8b7ff3a1b3c5ccd9a6b622de8db7057f9
treeinrandomforest <sra2114@gmail.com>
2021-12-13 16:30:49 -0500

    install script

---
+++ install_donkeycar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -uex
+
+dnf install pip -y
+pip install -r requirements.txt
+pip install git+https://github.com/tawnkramer/gym-donkeycar
+wget https://github.com/tawnkramer/gym-donkeycar/releases/download/v21.12.11/DonkeySimLinux.zip && unzip DonkeySimLinux.zip
+chmod u+x DonkeySimLinux/donkey_sim.x86_64
+DonkeySimLinux/donkey_sim.x86_64
+
+echo "IP:", ifconfig | grep inet
