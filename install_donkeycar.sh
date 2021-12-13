1b93fc329212a0807097359635c3c304f69cebe5
treeinrandomforest <sra2114@gmail.com>
2021-12-13 16:42:35 -0500

    sudo for dnf

--- install_donkeycar.sh
+++ install_donkeycar.sh
@@ -2,11 +2,12 @@
 
 set -uex
 
-dnf install pip -y
+sudo dnf install pip -y
 pip install -r requirements.txt
 pip install git+https://github.com/tawnkramer/gym-donkeycar
 wget https://github.com/tawnkramer/gym-donkeycar/releases/download/v21.12.11/DonkeySimLinux.zip && unzip DonkeySimLinux.zip
 chmod u+x DonkeySimLinux/donkey_sim.x86_64
+echo "IP:", ifconfig | grep inet
 DonkeySimLinux/donkey_sim.x86_64
 
-echo "IP:", ifconfig | grep inet
+
