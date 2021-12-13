e8d9d263cab6875e5b2319c6729387bc273476ba
treeinrandomforest <sra2114@gmail.com>
2021-12-13 16:49:22 -0500

    small fix

--- install_donkeycar.sh
+++ install_donkeycar.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 pip install git+https://github.com/tawnkramer/gym-donkeycar
 wget https://github.com/tawnkramer/gym-donkeycar/releases/download/v21.12.11/DonkeySimLinux.zip && unzip DonkeySimLinux.zip
 chmod u+x DonkeySimLinux/donkey_sim.x86_64
-echo "IP:", ifconfig | grep inet
+echo "IP:", `ifconfig | grep inet`
 DonkeySimLinux/donkey_sim.x86_64
 
 
